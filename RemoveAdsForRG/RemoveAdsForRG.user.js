4d7a78f948ef97d6e0d403bd71e1eca0e49797f8
xv <xiongyu132@gmail.com>
2021-01-28 17:36:30 +0800

    Create RemoveAdsForRG.user.js
    initial

---
+++ RemoveAdsForRG/RemoveAdsForRG.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         RemoveAdsForRG
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Remove Ads for refactoringguru.cn
+// @author       XVCoder
+// @license      GPL-3.0-only
+// @create       2021-01-28
+// @lastmodified 2021-01-28
+// @note         2021-01-28 v0.1 初始化
+// @match        https://refactoringguru.cn/*
+// @icon         https://xnu132.win/assets/img/favicon.png
+// @grant        none
+// ==/UserScript==
+
+
+// 根据网速自己设置时间间隔
+var interval = 3000;
+
+(function() {
+    'use strict';
+    setTimeout(function () {
+        var adsFromRight = document.getElementsByClassName("banner-inner")[0];//右侧书籍推广广告
+        var menuBrand = document.getElementsByClassName("menu-brand")[0];//左侧菜单品牌logo
+        var prom = document.getElementsByClassName("prom");//顶部和底部广告
+        adsFromRight.remove();
+        menuBrand.remove();
+        for (var i = (prom.length - 1); i >= 0; i--) {
+            prom[i].remove();
+        }
+    }, interval);
+})();
